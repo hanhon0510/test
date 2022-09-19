df75706712326ef3d698e71642daec8fe80bed43
hanhon0510 <tranhieuams@gmail.com>
2022-09-19 15:51:16 +0700

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,25 @@
+// Import your library
+// Do not change the name of the Solution class
+public class HelloWorld {
+    // Type your main code here
+    /**
+     * tim uoc chung lon nhat
+     */
+    public static int gcd(int a, int b) {
+        if (a < 0) {
+            a *= -1;
+        }
+        if (b < 0) {
+            b *= -1;
+        }
+        if (a % b == 0) {
+            return b;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(12, 16));
+    }
+}
